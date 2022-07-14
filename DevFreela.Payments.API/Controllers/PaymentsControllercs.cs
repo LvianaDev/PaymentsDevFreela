8312c7b39df06a6b3b4dd0611d68f61629db2a3c
LVIANACODE <lucas_viana99@outlook.com>
2022-07-13 22:14:15 -0300

    API and Payment Gateway from the DevFreela project

---
+++ DevFreela.Payments.API/Controllers/PaymentsControllercs.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.Payments.API.Controllers
+{   
+    [Route("api/payments")]
+    public class PaymentsControllercs
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] PaymentInfoInputModel paymentInfoInputModel)
+        {
+
+        }
+    }
+}
---
+++ DevFreela.Payments.API/Models/PaymentInfoInputModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.Payments.API.Models
+{
+    public class PaymentInfoInputModel
+    {
+    }
+}
